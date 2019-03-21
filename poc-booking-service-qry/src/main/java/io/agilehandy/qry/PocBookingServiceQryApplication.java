eed87de7081a6d46bd3a463c8dc870b077e2cc8d
Andy Xiao <andy.xiao@oocl.com>
2019-03-21 09:34:39 +0800

    qry no need discovery

--- poc-booking-service-qry/src/main/java/io/agilehandy/qry/PocBookingServiceQryApplication.java
+++ poc-booking-service-qry/src/main/java/io/agilehandy/qry/PocBookingServiceQryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+
 public class PocBookingServiceQryApplication {
 
 	public static void main(String[] args) {
